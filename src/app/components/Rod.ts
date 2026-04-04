efc9eef983063f9954ce91bcd92066cc2ae44fee
dragonspirals <leeradja99@gmail.com>
2026-04-04 19:35:02 +0100

    animate string without gravity

--- src/app/components/Rod.ts
+++ src/app/components/Rod.ts
@@ -1,44 +1,33 @@
 import { BoundedContainer } from "../displayElements/BoundedContainer";
-import { Magnitude, Position } from "../utils/Vector";
 import { BackpackItem } from "./BackpackItem";
 import * as PIXI from "pixi.js"
-import { RodSection } from "./ConnectedRods";
+import { RodSection } from "./RodSection";
 import { DynamicObjectSettings } from "./DynamicObject";
 
 export class Rod extends BackpackItem
 {
-    private _radius: number = 100
-    // private _endPos: Position = { x: 0, y: this._radius }
-    // private _rotVel: number = 0;
-    private _rodSection: RodSection
-    private _section2: RodSection
+    private _rodSections: RodSection[] = []
     private get graphics()
     {
         return this._object as PIXI.Graphics
     }
 
-    /**theta measured from vertical downwards */
-    // private _theta: number = 0;
-    // public set theta(val: number)
-    // {
-    //     this._theta = val % (Math.PI * 2)
-    //     this._endPos = { x: this._radius * Math.sin(this._theta), y: this._radius * Math.cos(this._theta)}
-    //     this.drawUpdate();
-    // }
-
     constructor(settings: DynamicObjectSettings)
     {
         super(settings)
-        this._rodSection = new RodSection();
-        this._section2 = new RodSection();
+        for (let i=0; i< 11; i++)
+        {
+
+            const rodSection = new RodSection();
+            this._rodSections.push(rodSection)
+        }
         this.drawUpdate();
     }
 
     public update(container: BoundedContainer)
     {
         super.update(container)
-        this._rodSection.update()
-        this._section2.update()
+        this._rodSections.forEach((section) => section.update())
         this.drawUpdate();
         
         // if (this._rotVel === 0 && this._theta === 0) { return; }
@@ -54,19 +43,28 @@ export class Rod extends BackpackItem
         // const distance = Magnitude(endDirection)
         // this._endPos = { x: endDirection.x * this._radius/distance, y: endDirection.y * this._radius/distance}
         // this._theta = Math.atan(this._endPos.x/Math.abs(this._endPos.y))
-        this._rodSection.updateStartPos(this.toLocal(e.global))
-        this._section2.updateStartPos(this._rodSection.endPos)
-        this.drawUpdate();    
+        this._rodSections[0].updateStartPos(this.toLocal(e.global))
+        for (let i=1; i<this._rodSections.length; i++)
+        {
+            this._rodSections[i].updateStartPos(this._rodSections[i-1].endPos)
+        }
+        // this._section2.updateStartPos(this._rodSection.endPos)
+        // this.drawUpdate();    
     }
 
     protected drawUpdate()
     {
+        console.log("drawing update")
         this.graphics.clear();
-        this.graphics.moveTo(this._rodSection.startPos.x, this._rodSection.startPos.y);
-        // if (!this._endPos) { this._endPos = { x: 0, y: 100 }}
-        this.graphics.lineTo(this._rodSection.endPos.x, this._rodSection.endPos.y)
-            .lineTo(this._section2.endPos.x, this._section2.endPos.y)
-            .stroke({ color: "#6b1000", width: 20 })
+        this.graphics.moveTo(this._rodSections[0].startPos.x, this._rodSections[0].startPos.y)
+            .lineTo(this._rodSections[0].endPos.x, this._rodSections[0].endPos.y)
+            // .stroke({ color: "#6b1000", width: 20 })
+
+        for (let i=1; i<this._rodSections.length; i++)
+        {
+            this.graphics.lineTo(this._rodSections[i].endPos.x, this._rodSections[i].endPos.y)
+                .stroke({ color: "#6b1000", width: 5 })
+        }
     }
 
     protected drawObject()
--- src/app/components/RodSection.ts
+++ src/app/components/RodSection.ts
@@ -2,7 +2,7 @@ import * as VECTOR from "../utils/Vector";
 
 export class RodSection
 {
-    private _radius: number = 100
+    private _radius: number = 10
     private _startPos: VECTOR.Position = { x: 0, y: 0 }
     public get startPos() : VECTOR.Position { return this._startPos}
     private _endPos: VECTOR.Position = { x: 0, y: this._radius }
@@ -21,11 +21,11 @@ export class RodSection
     public update()
     {
         // if (this._rotVel === 0 && this._theta === 0) { return; }
-        const direction = VECTOR.Subtract(this._endPos, this.startPos)
-        const theta = Math.atan(direction.x/Math.abs(direction.y))
-        const rotAccel = -0.1 * Math.sin(theta) 
-        const newTheta = theta + rotAccel
-        this._endPos = {x: this._radius * Math.sin(newTheta) + this._startPos.x, y:  this._radius * Math.cos(newTheta) + this._startPos.y}
+        // const direction = VECTOR.Subtract(this._endPos, this.startPos)
+        // const theta = Math.atan(direction.x/Math.abs(direction.y))
+        // const rotAccel = -0.1 * Math.sin(theta) 
+        // const newTheta = 0.98 * (theta + rotAccel)
+        // this._endPos = {x: this._radius * Math.sin(newTheta) + this._startPos.x, y:  this._radius * Math.cos(newTheta) + this._startPos.y}
 
     }
 
@@ -33,8 +33,11 @@ export class RodSection
     {
         const direction = VECTOR.Subtract(this._endPos, startPos)
         const distance = VECTOR.Magnitude(direction)
-        this._endPos = { x: direction.x * this._radius/distance + startPos.x, y: direction.y * this._radius/distance + startPos.y}
+        const newEndPos = { x: direction.x * this._radius/distance + startPos.x, y: direction.y * this._radius/distance + startPos.y}
         // this._theta = Math.atan(this._endPos.x/Math.abs(this._endPos.y))
         this._startPos = startPos
+        const movement = VECTOR.Subtract(this._endPos, newEndPos)
+        if (movement.x !== 0 || movement.y !== 0) { console.log(movement) }
+        this._endPos = newEndPos
     }
 }
